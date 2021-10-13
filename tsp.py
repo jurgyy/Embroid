42d28fa4767bb982e9565b2b1a3ee4d27f674bb5
Jurgen <jurgen.baas@outlook.com>
2021-10-13 21:24:53 +0200

    Replaced Travel.Alen with a pre-calculated distance matrix. Removed Travel.goodbad/goal since the distance matrix can only minimize the distance.

--- tsp.py
+++ tsp.py
@@ -11,22 +11,20 @@ import matplotlib.pyplot as plt
 
 
 class Travel:
-    goodbad = 1  # Maximise (1) or minimise (-1)
     showmoves = False  # Show moves ?
     tracing = False  # Trace solution ?
 
-    def __init__(self, goal, ncity):
+    def __init__(self, ncity):
         self.ncity = ncity
         self.x = np.zeros(self.ncity)  # Array of X co-ordinates
         self.y = np.zeros(self.ncity)  # Array of Y co-ordinates
         self.iorder = np.zeros(self.ncity, dtype=np.int32)  # Order of traversal
+        self.distance_matrix = np.zeros((self.ncity, self.ncity))
 
         # -----------
 
         self.nsucc = 0
         self.tfactr = 0.9  # Annealing schedule -- T reduced by this factor on each step.
-
-        self.goodbad = goal
 
         if Travel.tracing:
             print("Solving by simulated annealing: " + str(self.ncity) + " cities.")
@@ -41,15 +39,7 @@ class Travel:
         self.t = 0.5
 
         # Calculate length of initial path, wrapping circularly
-        self.path = 0.0
-        for i in range(self.ncity - 1):
-            i1 = self.iorder[i]
-            i2 = self.iorder[i + 1]
-            self.path += Travel.alen(self.x[i1], self.x[i2], self.y[i1], self.y[i2])
-
-        i1 = self.iorder[self.ncity - 1]
-        i2 = self.iorder[0]
-        self.path += Travel.alen(self.x[i1], self.x[i2], self.y[i1], self.y[i2])
+        self.path = float("inf")
 
         self.de = 0
         self.n = np.zeros(6, dtype=np.int32)
@@ -61,9 +51,12 @@ class Travel:
             self.y[i] = random.random()
             self.iorder[i] = i
 
-    def solve(self, goal, tracing):
+    def solve(self, tracing):
         """ Perform solution in one whack """
-        self.iorder = self.solve_tsp(goal, tracing)
+        coords = np.array([self.x, self.y]).T
+        self.distance_matrix = np.linalg.norm(coords[:, None, :] - coords[None, :, :], axis=-1)
+
+        self.iorder = self.solve_tsp(tracing)
 
     def show_path(self):
         """  Show the optimised path on the console log
@@ -76,7 +69,7 @@ class Travel:
         for i in range(self.ncity):
             ii = self.iorder[i]
             jj = self.iorder[0 if i == self.ncity - 1 else (i + 1)]
-            cost = Travel.alen(self.x[ii], self.x[jj], self.y[ii], self.y[jj])
+            cost = self.distance_matrix[ii][jj]
             print("     "
                   + Travel.ffixed(ii, 3, 0)
                   + "     "
@@ -110,18 +103,8 @@ class Travel:
         """ Return a random bit as a Boolean value """
         return random.random() < 0.5
 
-    @staticmethod
-    def alen(x1, x2, y1, y2):
-        """  Calculate logical length between two points in the plane taking into account whether we are minimising
-        or maximising travel distance"""
-        def sqr(x):
-            return x ** 2
-
-        return Travel.goodbad * (math.sqrt(sqr(x2 - x1) + sqr(y2 - y1)))
-
-    """  Perform one step of annealing.  Returns true when a
-        solution is found.  """
     def solve_step(self):
+        """  Perform one step of annealing.  Returns true when a solution is found.  """
         self.nsucc = 0
         for k in range(1, self.nover + 1):
             while True:
@@ -142,7 +125,7 @@ class Travel:
                 self.n[2] = (self.n[1] + (int(abs(nn - 2) * random.random())) + 1) % self.ncity
 
                 #  Calculate cost of transporting the segment
-                z = self.transport_cost(self.x, self.y, self.iorder, self.ncity, self.n)
+                z = self.transport_cost(self.distance_matrix, self.iorder, self.ncity, self.n)
                 self.de = z[0]
                 self.n = z[1]
                 if self.metropolis(self.de, self.t):
@@ -153,7 +136,7 @@ class Travel:
                     self.iorder = self.transport(self.iorder, self.ncity, self.n)
             else:
                 #  Reversal: calculate cost of reversing the segment
-                self.de = self.reversal_cost(self.x, self.y, self.iorder, self.ncity, self.n)
+                self.de = self.reversal_cost(self.distance_matrix, self.iorder, self.ncity, self.n)
                 if self.metropolis(self.de, self.t):
                     self.nsucc += 1
                     self.path += self.de
@@ -174,34 +157,25 @@ class Travel:
         return self.nsucc == 0  #
 
     @staticmethod
-    def reversal_cost(x, y, iorder, ncity, n):
+    def reversal_cost(distance_matrix, iorder, ncity, n):
         """
         Calculate cost function for a proposed path reversal.
         Returns the cost if the path n[0] to n[1] were to be
         reversed.
 
-        :param x: Array of city X co-ordinates
-        :param y: Array of city Y co-ordinates
+        :param distance_matrix: Distance matrix of shape (ncity, ncity) with the distance between each city
         :param iorder: Array containing current itinerary
         :param ncity: Number of cities
         :param n: Array n[1] = start city n[2] = end city
         :return:
         """
-        xx = np.zeros(6)
-        yy = np.zeros(6)
-
         n[2] = (n[0] - 1) % ncity  # City preceding n[0]
         n[3] = (n[1] + 1) % ncity  # City following n[1]
 
-        for j in range(0, 4):
-            ii = iorder[n[j]]
-            xx[j] = x[ii]
-            yy[j] = y[ii]
-
-        de = -Travel.alen(xx[0], xx[2], yy[0], yy[2]) - \
-             Travel.alen(xx[1], xx[3], yy[1], yy[3]) + \
-             Travel.alen(xx[0], xx[3], yy[0], yy[3]) + \
-             Travel.alen(xx[1], xx[2], yy[1], yy[2])
+        de = -distance_matrix[iorder[n[0]]][iorder[n[2]]] - \
+             distance_matrix[iorder[n[1]]][iorder[n[3]]] + \
+             distance_matrix[iorder[n[0]]][iorder[n[3]]] + \
+             distance_matrix[iorder[n[1]]][iorder[n[2]]]
 
         return de
 
@@ -218,7 +192,7 @@ class Travel:
         # This many cities must be swapped to effect the reversal.
         nn = (1 + ((n[1] - n[0] + ncity) % ncity)) / 2
 
-        for j in range(int(nn)):  # (j = 1; j <= nn; j++):
+        for j in range(int(nn)):
             # Start at the ends of the segment and swap pairs of cities, moving toward the center.
             k = (n[0] + j) % ncity
             l = (n[1] - j) % ncity
@@ -230,7 +204,7 @@ class Travel:
         return iorder
 
     @staticmethod
-    def transport_cost(x, y, iorder, ncity, n):
+    def transport_cost(distance_matrix, iorder, ncity, n):
         """
         Compute cost of transport along a given path.  Returns an array containing the cost and the updated path
         array in which:
@@ -243,34 +217,24 @@ class Travel:
         The values in the n[] array returned are used by transport() below to actually move the path if the
         decision is made to do so.
 
-        :param x: City X co-ordinates
-        :param y: City Y co-ordinates
+        :param distance_matrix: Distance matrix of shape (ncity, ncity) with the distance between each city
         :param iorder: Current itinerary
         :param ncity: Number of cities
         :param n: [0] Path start [1] Path end [2] Destination
         :return:
         """
-        xx = np.zeros(6)
-        yy = np.zeros(6)
-
         n[3] = (n[2] + 1) % ncity  # City following n[2]
         n[4] = (n[0] - 1) % ncity  # City preceding n[0]
         n[5] = (n[1] + 1) % ncity  # City following n[1]
 
-        #  Extract co-ordinates for the six cities involved
-        for j in range(6):
-            ii = iorder[n[j]]
-            xx[j] = x[ii]
-            yy[j] = y[ii]
-
         # Calculate the cost of disconnecting the path segment from n[0] to n[1], splicing it in between the two
         # adjacent cities n[2] and n[3], and connecting n[4] to n[5].
-        de = -Travel.alen(xx[1], xx[5], yy[1], yy[5]) - \
-            Travel.alen(xx[0], xx[4], yy[0], yy[4]) - \
-            Travel.alen(xx[2], xx[3], yy[2], yy[3]) + \
-            Travel.alen(xx[0], xx[2], yy[0], yy[2]) + \
-            Travel.alen(xx[1], xx[3], yy[1], yy[3]) + \
-            Travel.alen(xx[4], xx[5], yy[4], yy[5])
+        de = -distance_matrix[iorder[n[1]]][iorder[n[5]]] - \
+             distance_matrix[iorder[n[0]]][iorder[n[4]]] + \
+             distance_matrix[iorder[n[2]]][iorder[n[3]]] + \
+             distance_matrix[iorder[n[0]]][iorder[n[2]]] + \
+             distance_matrix[iorder[n[1]]][iorder[n[3]]] + \
+             distance_matrix[iorder[n[4]]][iorder[n[5]]]
 
         return de, n
 
@@ -326,29 +290,27 @@ class Travel:
         """
         return (de < 0) or (random.random() < math.exp(-de / t))
 
-    def solve_tsp(self, maxmin, tracing):
+    def solve_tsp(self, tracing):
         """
         Combinatorial minimisation by simulated annealing. Returns the optimised order array.
 
-        :param maxmin:
         :param tracing:
         :return:
         """
         if tracing:
             print(f"Solving by simulated annealing: {self.ncity} cities.")
 
-        self.goodbad = maxmin  # Set maximise / minimise
         self.showmoves = tracing
 
         # Calculate length of initial path
         for i in range(self.ncity - 1):  # (i = 1; i < ncity; i++):
             i1 = self.iorder[i]
             i2 = self.iorder[i + 1]
-            self.path += Travel.alen(self.x[i1], self.x[i2], self.y[i1], self.y[i2])
+            self.path += self.distance_matrix[i1][i2]
 
         i1 = self.iorder[self.ncity - 1]
         i2 = self.iorder[0]
-        self.path += Travel.alen(self.x[i1], self.x[i2], self.y[i1], self.y[i2])
+        self.path += self.distance_matrix[i1][i2]
 
         # Calculate length of initial path, wrapping circularly
         for j in range(self.ncity * 4):
@@ -365,15 +327,14 @@ class Travel:
 
 def main():
     random.seed(0)
-    cities = 10
-    goal = 1
-    t = Travel(goal, cities)
+    cities = 300
+    t = Travel(cities)
 
     tracing = False
     # t.newProblem(cities)
     t.place_cities()
     if True:
-        t.solve(goal, tracing)
+        t.solve(tracing)
 
     t.show_path()
 
