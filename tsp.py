ae5355b57bb0c13fa84ed55750059b000a8007cc
Jurgen <jurgen.baas@outlook.com>
2021-10-15 16:40:39 +0200

    Massively improved performance using Numba. However had to move a lot of functionality outside the class to get this to work.

--- tsp.py
+++ tsp.py
@@ -3,11 +3,219 @@
 """
 
 import math
-import random
+from numba import jit, float32, float64, int32, boolean
+import numba as nb
 
 import numpy as np
 
 import matplotlib.pyplot as plt
+
+
+@jit(nopython=True)
+def set_numba_seed(seed: int):
+    """
+    Numba uses a separate internal random state. This function sets that state's seed.
+
+    :param seed: The seed.
+    :rtype: None
+    """
+    np.random.seed(seed)
+
+
+@jit(float32(), nopython=True)
+def rand_float() -> float:
+    """
+    Using a static random float method so we can easily use numba's internal random state everywhere.
+
+    :return float: random float
+    """
+    return np.random.random()
+
+
+@jit(int32(int32[:], int32), nopython=True)
+def choose_start(n: np.ndarray, ncity: int) -> int:
+    """
+    Sets the first two indices of the path array in which:
+        n[0]    Start of path to be transported
+        n[1]    End of path to be transported
+    and return the length of the segment outside this path.
+
+    :param n: Path array
+    :param ncity: Total number of cities
+    :return: Number of cities outside the segment of the randomly chosen path
+    """
+    while True:
+        #  Randomly choose the start and end of the segment
+        n[0] = (int(ncity * rand_float()))
+        n[1] = (int((ncity - 1) * rand_float()))
+        if n[1] >= n[0]:
+            n[1] = n[1] + 1
+
+        #  Compute number of cities not on the segment
+        nn = 1 + ((n[0] - n[1] + ncity - 1) % ncity)
+
+        if nn >= 3:
+            break
+    return nn
+
+
+@jit(float32(float64[:, :], int32[:], int32, int32[:]), nopython=True)
+def transport_cost(distance_matrix, iorder, ncity, n):
+    """
+    Compute cost of transport along a given path.  Returns the cost and updates the path
+    array in which:
+        n[0]    Start of path to be transported
+        n[1]    End of path to be transported
+        n[2]    City after which path is to be spliced
+        n[3]    City before which the path is to be spliced
+        n[4]    City preceding the path
+        n[5]    City following the path
+    The values in the n[] array returned are used by transport() below to actually move the path if the
+    decision is made to do so.
+
+    :param distance_matrix: Distance matrix of shape (ncity, ncity) with the distance between each city
+    :param iorder: Current itinerary
+    :param ncity: Number of cities
+    :param n: [0] Path start [1] Path end [2] Destination
+    :return:
+    """
+    n[3] = (n[2] + 1) % ncity  # City following n[2]
+    n[4] = (n[0] - 1) % ncity  # City preceding n[0]
+    n[5] = (n[1] + 1) % ncity  # City following n[1]
+
+    # Calculate the cost of disconnecting the path segment from n[0] to n[1], splicing it in between the two
+    # adjacent cities n[2] and n[3], and connecting n[4] to n[5].
+    de = -distance_matrix[iorder[n[1]]][iorder[n[5]]] - \
+         distance_matrix[iorder[n[0]]][iorder[n[4]]] - \
+         distance_matrix[iorder[n[2]]][iorder[n[3]]] + \
+         distance_matrix[iorder[n[0]]][iorder[n[2]]] + \
+         distance_matrix[iorder[n[1]]][iorder[n[3]]] + \
+         distance_matrix[iorder[n[4]]][iorder[n[5]]]
+
+    return de
+
+
+@jit(float32(float64[:, :], int32[:], int32, int32[:]), nopython=True)
+def reversal_cost(distance_matrix, iorder, ncity, n):
+    """
+    Calculate cost function for a proposed path reversal.
+    Returns the cost if the path n[0] to n[1] were to be
+    reversed.
+
+    :param distance_matrix: Distance matrix of shape (ncity, ncity) with the distance between each city
+    :param iorder: Array containing current itinerary
+    :param ncity: Number of cities
+    :param n: Array n[1] = start city n[2] = end city
+    :return:
+    """
+    n[2] = (n[0] - 1) % ncity  # City preceding n[0]
+    n[3] = (n[1] + 1) % ncity  # City following n[1]
+
+    de = -distance_matrix[iorder[n[0]]][iorder[n[2]]] - \
+         distance_matrix[iorder[n[1]]][iorder[n[3]]] + \
+         distance_matrix[iorder[n[0]]][iorder[n[3]]] + \
+         distance_matrix[iorder[n[1]]][iorder[n[2]]]
+
+    return de
+
+
+@jit(boolean(float32, float32), nopython=True)
+def metropolis(de, t) -> bool:
+    """
+    Metropolis algorithm.  Returns a Boolean value which tells whether to accept a reconfiguration which leads to
+    a change de in the objective function e.  If de < 0, the change is obviously an improvement, so the return is
+    always true.  If de > 0, return true with probability e ^ (-de / t) where t is the current temperature in the
+    annealing schedule.
+
+    :param de:
+    :param t:
+    :return:
+    """
+    return (de < 0) or (rand_float() < math.exp(-de / t))
+
+
+@jit(boolean(), nopython=True)
+def irbit1():
+    """ Return a random bit as a Boolean value """
+    return rand_float() < 0.5
+
+
+@jit(int32(int32, int32, int32), nopython=True)
+def outside(n1: int, nn: int, ncity: int) -> int:
+    """
+    Given the endpoint of a segment and the number of cities outside of the path and the total number of cities, return
+    a random point outside that segment.
+
+    :param n1: Last point of the segment.
+    :param nn: Number of cities outside the segment.
+    :param ncity: Total number of cities.
+    :return: Random city outside the segment.
+    """
+    return (n1 + (int(abs(nn - 2) * rand_float())) + 1) % ncity
+
+
+@jit(int32[:](int32[:], int32, int32[:]), nopython=True)
+def transport(iorder, ncity, n):
+    """
+    Do the actual city transport.  Returns the updated iorder array.
+
+    :param iorder: Current itinerary
+    :param ncity: Number of cities
+    :param n: Path indices from transport_cost()
+    :return:
+    """
+    jorder = np.zeros(ncity, dtype=np.int32)
+
+    m1 = 1 + ((n[1] - n[0] + ncity) % ncity)  # Number of Cities from n[0] to n[1]
+    m2 = 1 + ((n[4] - n[3] + ncity) % ncity)  # Number of Cities from n[3] to n[4]
+    m3 = 1 + ((n[2] - n[5] + ncity) % ncity)  # Number of Cities from n[5] to n[2]
+
+    nn = 0
+    #  Copy the chosen segment
+    for j in range(m1):
+        jj = (n[0] + j) % ncity
+        jorder[nn] = iorder[jj]
+        nn += 1
+    if m2 > 0:
+        #  Copy the segment from n[3] to n[4]
+        for j in range(m2):
+            jj = (n[3] + j) % ncity
+            ind = iorder[jj]
+            jorder[nn] = ind
+            nn += 1
+
+    if m3 > 0:
+        # Copy the segment from n[5] to n[2]
+        for j in range(m3):
+            jj = (n[5] + j) % ncity
+            jorder[nn] = iorder[jj]
+            nn += 1
+    return jorder
+
+
+@jit(int32[:](int32[:], int32, int32[:]), nopython=True)
+def reverse(iorder, ncity: int, n):
+    """
+    Reverse a path segment.  Returns the updated iorder array with the specified segment reversed.
+
+    :param iorder: Current itinerary
+    :param ncity: Number of cities
+    :param n: Path to be reversed [0] = start, [1] = end
+    :return: Itinerary
+    """
+    # This many cities must be swapped to effect the reversal.
+    nn = (1 + ((n[1] - n[0] + ncity) % ncity)) / 2
+
+    for j in range(int(nn)):
+        # Start at the ends of the segment and swap pairs of cities, moving toward the center.
+        k = (n[0] + j) % ncity
+        l = (n[1] - j) % ncity
+
+        itmp = iorder[k]
+        iorder[k] = iorder[l]
+        iorder[l] = itmp
+
+    return iorder
 
 
 class Travel:
@@ -39,24 +247,23 @@ class Travel:
         self.t = 0.5
 
         # Calculate length of initial path, wrapping circularly
-        self.path = float("inf")
+        self.path = 0  # float("inf")
 
-        self.de = 0
         self.n = np.zeros(6, dtype=np.int32)
 
     def place_cities(self):
         """ Randomly position cities on the map and set initial order """
         for i in range(self.ncity):
-            self.x[i] = random.random()
-            self.y[i] = random.random()
+            self.x[i] = rand_float()
+            self.y[i] = rand_float()
             self.iorder[i] = i
 
-    def solve(self, tracing):
+    def solve(self):
         """ Perform solution in one whack """
         coords = np.array([self.x, self.y]).T
         self.distance_matrix = np.linalg.norm(coords[:, None, :] - coords[None, :, :], axis=-1)
 
-        self.iorder = self.solve_tsp(tracing)
+        self.iorder = self.solve_tsp()
 
     def show_path(self):
         """  Show the optimised path on the console log
@@ -98,211 +305,55 @@ class Travel:
             return str(n)
         return f"{n:{width}.{decimals}}"
 
+    # noinspection PyTypeChecker
     @staticmethod
-    def irbit1():
-        """ Return a random bit as a Boolean value """
-        return random.random() < 0.5
-
-    def solve_step(self):
+    @jit(nb.types.Tuple((boolean, float32, int32[:]))
+             (int32[:], int32, int32, int32, float32, float64[:, :], float32, int32[:]))
+    def solve_step(n, ncity, nover, nlimit, t, distance_matrix, path, iorder):
         """  Perform one step of annealing.  Returns true when a solution is found.  """
-        self.nsucc = 0
-        for k in range(1, self.nover + 1):
-            while True:
-                #  Randomly choose the start and end of the segment
-                self.n[0] = (int(self.ncity * random.random()))
-                self.n[1] = (int((self.ncity - 1) * random.random()))
-                if self.n[1] >= self.n[0]:
-                    self.n[1] = self.n[1] + 1
+        nsucc = 0
+        for _ in range(nover):
+            nn = choose_start(n, ncity)
 
-                #  Compute number of cities not on the segment
-                nn = 1 + ((self.n[0] - self.n[1] + self.ncity - 1) % self.ncity)
-
-                if nn >= 3:
-                    break
             """  Randomly decide whether to try reversing the segment or transporting it elsewhere in the path.  """
-            if Travel.irbit1():
+            if irbit1():
                 #  Transport: randomly pick a destination outside the path
-                self.n[2] = (self.n[1] + (int(abs(nn - 2) * random.random())) + 1) % self.ncity
+                n[2] = outside(n[1], nn, ncity)
 
-                #  Calculate cost of transporting the segment
-                z = self.transport_cost(self.distance_matrix, self.iorder, self.ncity, self.n)
-                self.de = z[0]
-                self.n = z[1]
-                if self.metropolis(self.de, self.t):
-                    self.nsucc += 1
-                    self.path += self.de
+                #  Calculate cost of transporting the segment and update the path
+                de = transport_cost(distance_matrix, iorder, ncity, n)
+
+                if metropolis(de, t):
+                    nsucc += 1
+                    path += de
 
                     #  Transport the segment
-                    self.iorder = self.transport(self.iorder, self.ncity, self.n)
+                    iorder = transport(iorder, ncity, n)
             else:
                 #  Reversal: calculate cost of reversing the segment
-                self.de = self.reversal_cost(self.distance_matrix, self.iorder, self.ncity, self.n)
-                if self.metropolis(self.de, self.t):
-                    self.nsucc += 1
-                    self.path += self.de
+                de = reversal_cost(distance_matrix, iorder, ncity, n)
+                if metropolis(de, t):
+                    nsucc += 1
+                    path += de
                     #  Reverse the segment
-                    self.iorder = self.reverse(self.iorder, self.ncity, self.n)
+                    iorder = reverse(iorder, ncity, n)
 
             """  If we've made sufficient successful changes, we're done at this temperature.  """
-            if self.nsucc > self.nlimit:
+            if nsucc > nlimit:
                 break
 
-        if Travel.tracing:
-            print("Temp = " + Travel.ffixed(self.t, 9, 6) +
-                  "  Cost = " + Travel.ffixed(self.path, 9, 6) +
-                  "  Moves = " + Travel.ffixed(self.nsucc, 5, 0))
+        # Return true if no moves (frozen)
+        return nsucc == 0, path, iorder
 
-        # Reduce temperature and return true if no moves (frozen)
-        self.t *= self.tfactr
-        return self.nsucc == 0  #
-
-    @staticmethod
-    def reversal_cost(distance_matrix, iorder, ncity, n):
-        """
-        Calculate cost function for a proposed path reversal.
-        Returns the cost if the path n[0] to n[1] were to be
-        reversed.
-
-        :param distance_matrix: Distance matrix of shape (ncity, ncity) with the distance between each city
-        :param iorder: Array containing current itinerary
-        :param ncity: Number of cities
-        :param n: Array n[1] = start city n[2] = end city
-        :return:
-        """
-        n[2] = (n[0] - 1) % ncity  # City preceding n[0]
-        n[3] = (n[1] + 1) % ncity  # City following n[1]
-
-        de = -distance_matrix[iorder[n[0]]][iorder[n[2]]] - \
-             distance_matrix[iorder[n[1]]][iorder[n[3]]] + \
-             distance_matrix[iorder[n[0]]][iorder[n[3]]] + \
-             distance_matrix[iorder[n[1]]][iorder[n[2]]]
-
-        return de
-
-    @staticmethod
-    def reverse(iorder, ncity: int, n):
-        """
-        Reverse a path segment.  Returns the updated iorder array with the specified segment reversed.
-
-        :param iorder: Current itinerary
-        :param ncity: Number of cities
-        :param n: Path to be reversed [0] = start, [1] = end
-        :return: Itinerary wi
-        """
-        # This many cities must be swapped to effect the reversal.
-        nn = (1 + ((n[1] - n[0] + ncity) % ncity)) / 2
-
-        for j in range(int(nn)):
-            # Start at the ends of the segment and swap pairs of cities, moving toward the center.
-            k = (n[0] + j) % ncity
-            l = (n[1] - j) % ncity
-
-            itmp = iorder[k]
-            iorder[k] = iorder[l]
-            iorder[l] = itmp
-
-        return iorder
-
-    @staticmethod
-    def transport_cost(distance_matrix, iorder, ncity, n):
-        """
-        Compute cost of transport along a given path.  Returns an array containing the cost and the updated path
-        array in which:
-            n[0]    Start of path to be transported
-            n[1]    End of path to be transported
-            n[2]    City after which path is to be spliced
-            n[3]    City before which the path is to be spliced
-            n[4]    City preceding the path
-            n[5]    City following the path
-        The values in the n[] array returned are used by transport() below to actually move the path if the
-        decision is made to do so.
-
-        :param distance_matrix: Distance matrix of shape (ncity, ncity) with the distance between each city
-        :param iorder: Current itinerary
-        :param ncity: Number of cities
-        :param n: [0] Path start [1] Path end [2] Destination
-        :return:
-        """
-        n[3] = (n[2] + 1) % ncity  # City following n[2]
-        n[4] = (n[0] - 1) % ncity  # City preceding n[0]
-        n[5] = (n[1] + 1) % ncity  # City following n[1]
-
-        # Calculate the cost of disconnecting the path segment from n[0] to n[1], splicing it in between the two
-        # adjacent cities n[2] and n[3], and connecting n[4] to n[5].
-        de = -distance_matrix[iorder[n[1]]][iorder[n[5]]] - \
-             distance_matrix[iorder[n[0]]][iorder[n[4]]] + \
-             distance_matrix[iorder[n[2]]][iorder[n[3]]] + \
-             distance_matrix[iorder[n[0]]][iorder[n[2]]] + \
-             distance_matrix[iorder[n[1]]][iorder[n[3]]] + \
-             distance_matrix[iorder[n[4]]][iorder[n[5]]]
-
-        return de, n
-
-    @staticmethod
-    def transport(iorder, ncity, n):
-        """
-        Do the actual city transport.  Returns the updated iorder array.
-
-        :param iorder: Current itinerary
-        :param ncity: Number of cities
-        :param n: Path indices from transport_cost()
-        :return:
-        """
-        jorder = np.zeros(ncity, dtype=np.int32)
-
-        m1 = 1 + ((n[1] - n[0] + ncity) % ncity)  # Number of Cities from n[0] to n[1]
-        m2 = 1 + ((n[4] - n[3] + ncity) % ncity)  # Number of Cities from n[3] to n[4]
-        m3 = 1 + ((n[2] - n[5] + ncity) % ncity)  # Number of Cities from n[5] to n[2]
-
-        nn = 0
-        #  Copy the chosen segment
-        for j in range(m1):
-            jj = (n[0] + j) % ncity
-            jorder[nn] = iorder[jj]
-            nn += 1
-        if m2 > 0:
-            #  Copy the segment from n[4] to n[5]
-            for j in range(m2):
-                jj = (n[3] + j) % ncity
-                ind = iorder[jj]
-                jorder[nn] = ind
-                nn += 1
-
-        if m3 > 0:
-            # Copy the segment from n[6] to n[3]
-            for j in range(m3):
-                jj = (n[5] + j) % ncity
-                jorder[nn] = iorder[jj]
-                nn += 1
-        return jorder
-
-    @staticmethod
-    def metropolis(de, t) -> bool:
-        """
-        Metropolis algorithm.  Returns a Boolean value which tells whether to accept a reconfiguration which leads to
-        a change de in the objective function e.  If de < 0, the change is obviously an improvement, so the return is
-        always true.  If de > 0, return true with probability e ^ (-de / t) where t is the current temperature in the
-        annealing schedule.
-
-        :param de:
-        :param t:
-        :return:
-        """
-        return (de < 0) or (random.random() < math.exp(-de / t))
-
-    def solve_tsp(self, tracing):
+    def solve_tsp(self):
         """
         Combinatorial minimisation by simulated annealing. Returns the optimised order array.
-
-        :param tracing:
-        :return:
         """
-        if tracing:
+        if Travel.tracing:
             print(f"Solving by simulated annealing: {self.ncity} cities.")
 
-        self.showmoves = tracing
-
         # Calculate length of initial path
+        self.path = 0
         for i in range(self.ncity - 1):  # (i = 1; i < ncity; i++):
             i1 = self.iorder[i]
             i2 = self.iorder[i + 1]
@@ -314,7 +365,11 @@ class Travel:
 
         # Calculate length of initial path, wrapping circularly
         for j in range(self.ncity * 4):
-            if self.solve_step():
+            frozen, self.path, self.iorder = self.solve_step(self.n, self.ncity, self.nover, self.nlimit, self.t,
+                                                             self.distance_matrix, self.path, self.iorder)
+            self.t *= self.tfactr
+
+            if frozen:
                 break
 
         if Travel.tracing:
@@ -326,17 +381,17 @@ class Travel:
 
 
 def main():
-    random.seed(0)
-    cities = 300
+    set_numba_seed(0)
+
+    cities = 1000
     t = Travel(cities)
 
-    tracing = False
     # t.newProblem(cities)
     t.place_cities()
     if True:
-        t.solve(tracing)
+        t.solve()
 
-    t.show_path()
+    # t.show_path()
 
 
 if __name__ == '__main__':
